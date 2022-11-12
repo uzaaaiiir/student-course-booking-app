739f514b9e804603a0cd30f58e23f58da348404f
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-11-11 22:51:23 -0500

    Refactor of ManageStudentsActivity

--- app/src/main/java/com/example/coursebookingapp/activities/ManageStudentsActivity.java
+++ app/src/main/java/com/example/coursebookingapp/activities/ManageStudentsActivity.java
@@ -21,14 +21,11 @@ import com.example.coursebookingapp.user.User;
 import java.util.List;
 
 public class ManageStudentsActivity extends AppCompatActivity {
-    DatabaseHandler dbHandler = new DatabaseHandler(ManageStudentsActivity.this);
     ArrayAdapter studentsArrayAdapter;
-    User user;
     Button logout, search, back;
     ListView list;
     EditText searchBox;
     TextView welcome;
-    String searchCharacters;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -36,23 +33,14 @@ public class ManageStudentsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_students);
 
-        logout = findViewById(R.id.logout2);
-        search = findViewById(R.id.search);
-        searchBox = findViewById(R.id.searchStudents);
-        list = findViewById(R.id.listOfStudents);
-        welcome = findViewById(R.id.welcome);
-        back = findViewById(R.id.backBtn);
-
-        generateUser();
-        welcome.setText("Welcome, " + user.getUsername() + "\n" +
-                " (" +user.getRole()+ ")");
+        assignInputs();
+        setWelcomeText();
 
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DatabaseHandler dbHandler = new DatabaseHandler(ManageStudentsActivity.this);
-                searchCharacters = searchBox.getText().toString();
-                updateListView(dbHandler);
+                String searchCharacters = searchBox.getText().toString();
+                updateListView(searchCharacters);
             }
         });
 
@@ -60,54 +48,54 @@ public class ManageStudentsActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 User userDeleted = (User) parent.getItemAtPosition(position);
-                dbHandler.deleteUser(userDeleted);
-                Toast.makeText(ManageStudentsActivity.this, "Student Deleted: " + userDeleted.getUsername(), Toast.LENGTH_SHORT).show();
-                updateListView(dbHandler);
+                userDeleted.deleteUser(ManageStudentsActivity.this);
+                printMessage("Student Deleted: " + userDeleted.getUsername());
+                updateListView("");
             }
         });
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startMainActivity();
+                startNewActivity(MainActivity.class);
             }
         });
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startLoginActivity();
+                startNewActivity(LoginActivity.class);
             }
         });
 
     }
 
-    private void startLoginActivity() {
-        Intent loginActivity = new Intent(ManageStudentsActivity.this, LoginActivity.class);
-        startActivity(loginActivity);
-    }
-    private void startMainActivity() {
-        Intent mainActivity = new Intent(ManageStudentsActivity.this, MainActivity.class);
-        mainActivity.putExtra("Username", user.getUsername());
-        mainActivity.putExtra("Password", user.getPassword());
-        mainActivity.putExtra("Role", user.getRole());
-        startActivity(mainActivity);
+    private void assignInputs() {
+        logout = findViewById(R.id.logout2);
+        search = findViewById(R.id.search);
+        searchBox = findViewById(R.id.searchStudents);
+        list = findViewById(R.id.listOfStudents);
+        welcome = findViewById(R.id.welcome);
+        back = findViewById(R.id.backBtn);
     }
 
-    private void updateListView(DatabaseHandler dbHandler) {
-        List<User> studentsWithSearchCharacters = dbHandler.allStudents(searchCharacters);
+    private void setWelcomeText() {
+        welcome.setText("Welcome, " + User.getCurrentUser().getUsername() + "\n" +
+                " (" + User.getCurrentUser().getRole() + ")");
+    }
+
+    private void printMessage(String message) {
+        Toast.makeText(ManageStudentsActivity.this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void startNewActivity(Class activityToStart) {
+        Intent activity = new Intent(ManageStudentsActivity.this, activityToStart);
+        startActivity(activity);
+    }
+
+    private void updateListView(String searchCharacters) {
+        List<User> studentsWithSearchCharacters = User.getAllInstructors(ManageStudentsActivity.this ,searchCharacters);
         studentsArrayAdapter = new ArrayAdapter<User>(ManageStudentsActivity.this, android.R.layout.simple_list_item_1, studentsWithSearchCharacters);
         list.setAdapter(studentsArrayAdapter);
-    }
-
-
-    private void generateUser() {
-        Intent intent = getIntent();
-        Bundle bundle = intent.getExtras();
-
-        if (bundle != null) {
-            user = new User(-1, bundle.getString("Username"), bundle.getString("Password"), bundle.getString("Role"));
-
-        }
     }
 }
